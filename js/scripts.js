23c921540205f9971a674ddba46034b9e6838506
Jason M <jbird206@gmail.com>
2020-01-31 11:59:41 -0800

    Add files via upload
    completed front end interface, Jquery connects HTML order forms via JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,26 @@
-// $(document).ready(function() {
-//   $("form#calculator").submit(function() {
-//     event.preventDefault();
-//     var number1 = parseInt($("#input1").val());
-//     var number2 = parseInt($("#input2").val());
-//     var number3 = parseInt($("#input3").val());
-//     var number4 = parseInt($("#input4").val());
-//     var number5 = parseInt($("#input5").val());
-//
-//     var result = number1 + number2 + number3 + number4 + number5;
-//
-//     $("#output").text(result);
-//     if (result < 10) {
-//       $('#python').show();
-//       $("form#calculator").hide()
-//     } else if (result >= 10 & result < 19) {
-//       $('#Ruby').show();
-//       $("form#calculator").hide()
-//     } else if (result >= 19 & result < 30) {
-//       $('#C').show();
-//       $("form#calculator").hide()
-//     } else if (result >= 30 & result < 40) {
-//       $('#Java').show();
-//       $("form#calculator").hide()
-//     } else {
-//       $('#fullstack').show();
-//       $("form#calculator").hide()
-//     }
-//
-//   });
-// });
+
 ////////////////////// user Interface///////////////////
 //var pizzaOrder = new Pizza();
 
 $(document).ready(function() {
-  attachPizzaListeners();
   $("form#toppings").submit(function(event) {
-    event.preventDeafault();
-$("input:checkbox[name=pepperoni]:checked").each(function(){
-  var pepperoni =$(this).val();
+    event.preventDefault();
+    $("#selected-toppings").show();
+$("input:checkbox[name=pizza-toppings]:checked").each(function(){
+  var selectedPizzaToppings = $(this).val();
+  $('#selected-toppings').append(selectedPizzaToppings + "<br>");
+});
+$('#toppings').hide();
+});
+//size menu/////////////////////
+$("form#pizzaSize").submit(function(event) {
+  event.preventDefault();
+  $("#selected-size").show();
+  $("input:checkbox[name=size-selected]:checked").each(function(){
+    var selectedPizzaSize = $(this).val();
+    $('#selected-size').append(selectedPizzaSize + "<br>");
+  });
+  $('#pizzaSize').hide();
+  });
 
-
-
-    var inputToppingPep = $("input#toppingPepp").val();
-    var inputToppingSasg = $("input#toppingSasg").val();
-    var inputToppingOyst = $("input#toppingOyst").val();
-    var inputToppingAnch = $("input#toppingAnch").val();
-
-  }
-}
+});
